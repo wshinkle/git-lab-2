f780068c63d5ec73476772610abb58f549808045
Wyatt Shinkle <ws533420@ohio.edu>
2022-01-21 14:35:41 -0500

    message

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Wyatt Shinkle
+ *        Date: 1/21/22
+ * Description: git-lab c++ program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
